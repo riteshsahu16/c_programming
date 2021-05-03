202f95b3c58488b54729bb79b7d6f4c5c45bbe10
riteshsahu16 <riteshsahu0116@gmail.com>
2021-05-03 21:14:10 +0530

    added word counting example

---
+++ Section1/examples/eg_7_wordcounting.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#define IN 1
+#define OUT 0
+int main()
+{
+	//count lines, words and characters in input 
+	int nw, nl, nc, ch, state;
+	nw =1 ; nl = nc = 0;
+	
+	while((ch = getchar()) != EOF)
+	{
+		nc++;
+
+		if(ch == '\n')
+			nl++;
+		if(ch == ' ' || ch == '\t' || ch == '\n')
+		{
+			state = OUT;
+		}
+		else if(state==OUT)
+		{
+			state = IN;
+			nw++;
+		}
+	}
+
+	printf("Total No. of characters : %d\n", nc);
+	printf("Total no. of lines : %d\n", nl);
+	printf("Total no. of WORDS: %d\n", nw);
+	return 0;
+}
