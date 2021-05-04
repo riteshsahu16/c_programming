976a2569c2970f64336103556c504ab4e181698f
riteshsahu16 <riteshsahu0116@gmail.com>
2021-05-04 20:04:27 +0530

    added histogram

--- Section1/exercises/ex13_histogram_len_words.c
+++ Section1/exercises/ex13_histogram_len_words.c
@@ -1,17 +1,47 @@
 #include<stdio.h>
+#define MAX 500
 int main()
 {
-	//Draw histogram for given data
-	int data[] = {5,2,7,8,9};
-	n = 5;
-	//Horizontal histogram
-	int i, j;
+	int ch, n, data[MAX], len;
+	while ((ch=getchar()) != EOF)
+	{
+		//get length of words and put it in array
 	
+	}
+
+	//Draw histogram for given data : freq. for words of length 1, 2, 3, 4, ..... 
+	
+	
+	//Horizontal histogram
+	int i, j, max=0;
 	for(i=n; i>0; i--)
 	{
-		printf("%d|");
-		for(j=0; j<data[n-1]; j++)
+		if (i>max)
+			max = data[i-1];
+		
+		printf("%d|",i);
+		for(j=0; j<data[i-1]; j++)
 			printf("-");
 		printf("\n");
 	}
-}	
+	printf("\n\n");
+	//Vertical histogram
+	for(i=max; i>0; i--)
+	{
+		//printf("|");
+		for(j=0; j<n; j++)
+		{
+			if(data[j] >= i)
+				printf("| ");
+			else
+				printf("  ");
+		}
+		printf("\n");
+	}
+	for(i=0; i<2*n; i++)
+		printf("-");
+	printf("\n");
+	for(i=1; i<=n; i++)
+		printf("%d ", i);
+	printf("\n");
+}
