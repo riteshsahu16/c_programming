8df4858c266ece6600202f0d203431ba822c3367
riteshsahu16 <riteshsahu0116@gmail.com>
2021-05-05 08:49:45 +0530

    added ex13

--- Section1/exercises/ex13_histogram_len_words.c
+++ Section1/exercises/ex13_histogram_len_words.c
@@ -1,31 +1,72 @@
 #include<stdio.h>
 #define MAX 500
+#define IN 1
+#define OUT 0
 int main()
 {
-	int ch, n, data[MAX], len;
+	int ch, n=0, data[MAX], len=0, state = OUT, i, j=0, max=0;
+	
+	for(i=0; i<MAX; i++)
+	{
+		data[i] = 0;
+	}
+	
 	while ((ch=getchar()) != EOF)
 	{
-		//get length of words and put it in array
-	
-	}
 
+		//get length of words and put it in array
+		if(ch == ' ' || ch == '\t' || ch=='\n')
+		{
+			if(state==IN)
+			{
+				n++;
+				data[j] = len;
+				j++;
+				state = OUT;
+				len = 0;
+			}	
+		}
+		else
+		{
+			if(state == IN)
+				len++;
+			else
+			{
+				state = IN;
+				len++;
+			}
+		}
+	}
+	
+	//print data
+	printf("data : \n");
+	for(i = 0; i<n; i++)
+	{
+		printf("%d : %d\n", i+1, data[i]);
+		if(data[i]>max)
+			max = data[i];
+	}
+	//printf("largest word length: %d\n", max);
+	
 	//Draw histogram for given data : freq. for words of length 1, 2, 3, 4, ..... 
-	
-	
+	i=j=0;
+	printf("\n\n");
+	printf("X-axis :length-word \t Y-axis :word-position\n");
+	printf("Horizontal Histogram : \n\n");	
 	//Horizontal histogram
-	int i, j, max=0;
 	for(i=n; i>0; i--)
 	{
-		if (i>max)
-			max = data[i-1];
-		
 		printf("%d|",i);
-		for(j=0; j<data[i-1]; j++)
+		for(j=0; j<=data[i-1]; j++)
 			printf("-");
 		printf("\n");
 	}
 	printf("\n\n");
+	printf("X-axis :word-position \t  Y-axis:length-word\n");
+	printf("Vertical Histogram : \n\n");
+	
 	//Vertical histogram
+	
 	for(i=max; i>0; i--)
 	{
 		//printf("|");
@@ -44,4 +85,5 @@ int main()
 	for(i=1; i<=n; i++)
 		printf("%d ", i);
 	printf("\n");
+
 }
---
+++ Section1/exercises/ex14_histogram_frequencies_char.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define SIZE 128
+int main()
+{
+	//Print a histogram of the frequencies of different characaters in its input.
+	
+	//1. Input all characters & store it's frequency in an array
+	
+
+
+	//2. Print histogram a/c to arr: if any entry's value is not greater than zero than ignore that entry.
+	
+
+}
+
