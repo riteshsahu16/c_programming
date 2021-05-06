14b58cd4e2c5f11ad1f9a8c4af6bd632bc21a85e
riteshsahu16 <riteshsahu0116@gmail.com>
2021-05-06 21:36:10 +0530

    added pascal triangle

--- Section1/experiments/pascal_triangle.c
+++ Section1/experiments/pascal_triangle.c
@@ -1,18 +1,41 @@
 #include<stdio.h>
 #define N 8 // Height = base
+
+int factorial(int n)
+{
+	if (n==0 || n==1)
+		return 1;
+	else
+		return n * factorial(n-1);
+}
+
 int main()
 {
-<F7>	int i, j, h, c=0;
+	int i, j, h, c=0, val;
 	h = N-1;
-	for(i=N; i>0; i-- )
+	for(i=0; i<N; i++)
 	{
-		for(j=1; j<i; j++)
-			printf(" ",j);
-		printf("1");
+		//space
+		for(j=1; j<N-i; j++)
+			printf(" ");
+
+		for(j=0; j<=i; j++)
+		{
+			if(i==0)
+				printf("1");
+			else 
+			{
+				val = factorial(i) / (factorial(i-j) * factorial(j)) ;
+				printf("%d ", val);
 		
-		for(j=1; j<i; j++)
-			printf(" ", j);
+			}
+		}
 		
+		//space
+		for(j=1; j<N-i; j++)
+		{
+			printf(" ");
+		}
 		printf("\n");
-	}
+	}	
 }
